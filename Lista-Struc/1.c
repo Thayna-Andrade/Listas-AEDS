5a3632cdcfa844241218272f58d57c69a8835704
Thayna-Andrade <thayyyandrade@gmail.com>
2025-06-07 22:08:51 +0000

    Lista Structs

---
+++ Lista-Struc/1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define MAX 100
+
+struct pessoa
+{
+    char nome[MAX];
+    float altura;
+    int datanasc;
+};
+
+int datadenascimento(int datanasc){
+
+}
+
+int main(){
+    struct pessoa Pessoas[10];
+    for(int i = 0; i < 10; i++){
+        printf("Digite o nome: ");
+
+    }
+}
